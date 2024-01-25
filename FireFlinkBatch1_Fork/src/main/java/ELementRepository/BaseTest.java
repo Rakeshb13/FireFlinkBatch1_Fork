53f748ce8254bf4a6402c255b495ccd10885d179
Rakesh B <Rakesh B@TYSS-Rakesh-B>
2024-01-25 12:34:21 +0530

    First Commit fork

---
+++ FireFlinkBatch1_Fork/src/main/java/ELementRepository/BaseTest.java
@@ -0,0 +1,18 @@
+package ELementRepository;
+
+public class BaseTest {
+	
+//	Hi Team...
+	
+	public void launchBrowser123()
+	{
+		//by Rakesh
+	}
+
+	public void launchBrowser()
+	{
+		//by Rakesh
+	}
+}
+
+
